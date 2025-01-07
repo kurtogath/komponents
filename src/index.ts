49930455265fafb0e8f6f49d72c927534d06f40e
Oscar <kurtogath@gmail.com>
2025-01-07 13:25:44 +0100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './components'; // This will export all components from the 'components' folder
+
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import { peerDependencies } from "./package.json";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "./src/index.ts", // Specifies the entry point for building the library.
+      name: "vite-react-ts-button", // Sets the name of the generated library.
+      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
+      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+    },
+    sourcemap: true, // Generates source maps for debugging.
+    emptyOutDir: true, // Clears the output directory before building.
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+  },
+});
+
